55d708aada84878ee65b053b100d638201bef034
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-03-23 14:29:12 +0100

    appveyor: use relative paths for assets

--- msys_build.sh
+++ msys_build.sh
@@ -6,14 +6,17 @@ cd /c/projects/Stockfish/src
 make clean
 make profile-build COMP=mingw ARCH=x86-64
 strip stockfish.exe
-mv stockfish.exe stockfish-windows-x86_64.exe
+mv stockfish.exe ../stockfish-windows-x86_64.exe
 
 make clean
 make profile-build COMP=mingw ARCH=x86-64-modern
 strip stockfish.exe
-mv stockfish.exe stockfish-windows-x86_64-modern.exe
+mv stockfish.exe ../stockfish-windows-x86_64-modern.exe
 
 make clean
 make profile-build COMP=mingw ARCH=x86-64-bmi2
 strip stockfish.exe
-mv stockfish.exe stockfish-windows-x86_64-bmi2.exe
+mv stockfish.exe ../stockfish-windows-x86_64-bmi2.exe
+
+cd /c/projects/Stockfish/
+ls -lh
